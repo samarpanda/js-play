d33ac134f73a85a4570a494e58d0200a02949b67
Samar Panda <samar.panda84@gmail.com>
2015-09-26 23:31:56 +0530

    Regx practise

--- server/src/regx.js
+++ server/src/regx.js
@@ -3,8 +3,14 @@ var l = console.log;
 var regx = function(){
 
 	return {
+		// 2015-12-23
 		'rdate': /^\d{4}-(0\d|1[0-2])-([0-2]\d|3[0-1])$/,
-		'hdec': /^#([a-f\d]{3}){1,2}$/
+		// #123abc, #fab
+		'hdec': /^#(?:[a-f\d]{3}){1,2}$/,
+		// .5, +1.5, -1., 25
+		'num': /^[+-]?(?:\d*\.?\d+|\d+\.)/g,
+		// sp foo dsp
+		'word': /\bfoo\b/
 	};
 }();
 
--- server/test/regx.spec.js
+++ server/test/regx.spec.js
@@ -6,32 +6,77 @@ var l = console.log;
 var regx = reg.regx;
 var ureg = reg.uregx;
 
-var regTest = function(reg, arr){
+var regPassAllContains = function(reg, arr){
 	return arr.every(function(el){
-		return el.match(reg);
+		// l(el.search(reg), el);
+		return el.search(reg) > -1;
 	});
 };
 
+var regPassAll = function(reg, arr){
+	return arr.every(function(el){
+		// l(el.search(reg), el);
+		return el.search(reg) === 0;
+	});
+};
+
+var regFailAll = function(reg, arr){
+	return arr.some(function(el){
+		// l(el.search(reg), el);
+		return el.search(reg) === 0;
+	});
+};
+
+/**
+ * Simple curry
+ */
 var keepT = function(regx){
-	return function(arr){
-		return regTest(regx, arr);
+	return function(arr, val){
+		var fn;
+		switch(val){
+			case true:
+				fn = regPassAll;
+			break;
+			case false:
+				fn = regFailAll;
+			break;
+			case 2://Contains
+				fn = regPassAllContains;
+			break;
+			default:
+				fn = regPassAll;
+			break;
+		}
+		return fn(regx, arr);
 	};
 };
 
 test('Is date', function(t){
 	var tReg = keepT(regx.rdate);
-	//Valid date
-	t.is(tReg(['2015-12-23']), true);
-	//Invalid date
-	// t.is(tReg(['2015-13-20', '2015-12-33']), false);
+	t.true(tReg(['2015-12-23'], true));
+	t.false(tReg(['2015-13-20', '2015-12-33'], false));
 	t.end();
 });
 
 test('Is hexadecimal', function(t){
 	var tReg = keepT(regx.hdec);
-	//Valid hexadecimal
-	t.is(tReg(['#fab', '#ccc', '#123', '#123abc']), true);
-	//Invalid hexadecimal
-	// t.is(tReg(['#fab1', '#ccc2', '#12323', '#ab']), false);
+	t.true(tReg(['#fab', '#ccc', '#123', '#123abc'], true));
+	t.false(tReg(['#fab1', '#ccc2', '#1233', '#ab'], false));
+	t.end();
+});
+
+test('Is Integer', function(t){
+	var tReg = keepT(regx.num);
+	t.true(tReg(['.5', '+1.5', '-1.', '25', '0.25'], true));
+	t.false(tReg(['--5.0', '++2.-', '+-0.2'], false));
+
+	//To fix this False positive
+	t.true(tReg(['5..0'], true));
+	t.end();
+});
+
+test('Contains word', function(t){
+	var tReg = keepT(regx.word);
+	t.true(tReg(['sp foo', 'foo sp', 'dsp foo sp'], 2));
 	t.end();
 });
